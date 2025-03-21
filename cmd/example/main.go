ef4276595620845d72354b4e23fcc2dbe5aa81f9
Max Netyaga <max.netyaga@gmail.com>
2025-03-21 23:10:47 +0200

    add cli interface

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,30 +3,62 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"os"
+	"strings"
+
 	lab2 "github.com/maxnetyaga/software-architecture-lab2"
 )
 
-var (
-	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
-)
-
 func main() {
+	expressionFlag := flag.String("e", "", "Expression for calculation")
+	fileFlag := flag.String("f", "", "File with expression for calculation")
+	outputFlag := flag.String("o", "", "File for recording the result")
+
 	flag.Parse()
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
-
-	postfix := "3 4 + 5 *" // (3 + 4) * 5
-	infix, err := lab2.PostfixToInfix(postfix)
-	if err != nil {
-		fmt.Println("Помилка:", err)
-		return
+	if *expressionFlag != "" && *fileFlag != "" {
+		fmt.Fprintln(os.Stderr, "Error: cannot use both -e and -f flags at the same time")
+		os.Exit(1)
 	}
-	fmt.Println("Інфіксний вираз:", infix)
+
+	var input io.Reader
+	if *expressionFlag != "" {
+		input = strings.NewReader(*expressionFlag)
+	} else if *fileFlag != "" {
+		file, err := os.Open(*fileFlag)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: Could not open file: %v\n", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		input = file
+	} else {
+		fmt.Fprintln(os.Stderr, "Error: no input expression specified (use -e or -f)")
+		os.Exit(1)
+	}
+
+	var output io.Writer
+	if *outputFlag != "" {
+		file, err := os.Create(*outputFlag)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: Failed to create file for result: %v\n", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		output = file
+	} else {
+		output = os.Stdout
+	}
+
+	handler := &lab2.ComputeHandler{
+		Input:  input,
+		Output: output,
+	}
+
+	err := handler.Compute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
 }
