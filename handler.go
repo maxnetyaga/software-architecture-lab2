0b692a9478e6ac7a8b9ff298abcfe53fa9d4738c
KostianDev <143603709+KostianDev@users.noreply.github.com>
2025-03-21 22:15:17 +0200

    Remove unnecessary comments

--- handler.go
+++ handler.go
@@ -6,16 +6,11 @@ import (
 	"strings"
 )
 
-// ComputeHandler should be constructed with input io.Reader and output io.Writer.
-// Its Compute() method should read the expression from input and write the computed result to the output.
-
-// Обробляє вхідні дані та записує результат
 type ComputeHandler struct {
 	Input  io.Reader
 	Output io.Writer
 }
 
-// Читає вхідні дані, викликає функцію з implementation.go та записує результат
 func (ch *ComputeHandler) Compute() error {
 	data, err := ioutil.ReadAll(ch.Input)
 	if err != nil {
